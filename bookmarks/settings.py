c4b9393574f67c7100fb5aca0f0d14c83ff0da7e
appyens <an.anurag@msn.com>
2020-01-26 23:57:22 +0530

    followers added

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -143,8 +143,8 @@ ABSOLUTE_URL_OVERRIDES = {
 # for smtp email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'an.anuraag@gmail.com'
-EMAIL_HOST_PASSWORD = '3UnTy!3###3'
+EMAIL_HOST_USER = 'email here'
+EMAIL_HOST_PASSWORD = 'password here'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
